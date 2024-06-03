3ca21deea2e480039b1b5f48834128005166f3df
YimiHuang <62888528+yiyihum@users.noreply.github.com>
2024-06-04 00:10:29 +0800

    Delete benchmark/results/azure-test directory

--- benchmark/results/azure-test/ml-binary-001/predict_sentiment.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Remove the first line (header) and then process the rest
-tail -n +2 test.csv | awk -F, 'BEGIN {OFS=","; print "Index,Label"} {if ($4 == "Student" && $5 == "No Income") print NR, "Positive"; else print NR, "Negative";}' > result.csv
