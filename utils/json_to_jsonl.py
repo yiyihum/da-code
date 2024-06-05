3ac90c4483552d490072576e69445fbdbf2f1e65
Stewie Peter <2577442909@qq.com>
2024-06-05 19:21:55 +0800

    add evaluation example

--- utils/json_to_jsonl.py
+++ utils/json_to_jsonl.py
@@ -1,14 +1,15 @@
 import json
 import jsonlines, os
 
-dir_path = 'evaluation_examples/examples/machinelearning'
+dir_path = 'evaluation_examples/examples/datavisualization'
 files = [os.path.join(dir_path, file) for file in os.listdir(dir_path)]
+files = sorted(files)
 evals = []
 for file in files:
     with open(file, 'r') as f:
         eval_dict = json.load(f)
     evals.append(eval_dict)
 
-with jsonlines.open('evaluation_examples/examples/machinelearning/Evaluation_Competition.jsonl', 'w') as f:
+with jsonlines.open('evaluation_examples/examples/machinelearning/Evaluation_Visual.jsonl', 'w') as f:
     for item in evals:
         f.write(item)
