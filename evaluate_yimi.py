db3b75286781bea1c56fdadb100ea406cb6e1427
yiyihum <1203218616@qq.com>
2024-06-10 13:30:11 +0000

    refine gold

--- evaluate_yimi.py
+++ evaluate_yimi.py
@@ -3,14 +3,20 @@ from tqdm import tqdm
 from spider2.evaluators.evaluation import Evaluator
 import json
 
-def run_evaluation(output_dir, gold_dir, eval_json, result_json, timeout_seconds=10):
+def run_evaluation(output_dir, gold_dir, eval_json, output_file, timeout_seconds=10):
     # Initialize the Evaluator with the provided directories
     evaluator = Evaluator(output_dir=output_dir, gold_dir=gold_dir, timeout_second=timeout_seconds)
 
     # Perform the evaluation
     results_infos = evaluator.evaluate(env_config=eval_json)
-    with open(result_json, 'w') as json_file:
-        json.dump(results_infos, json_file, indent=4)
+    num_results = len(results_infos)
+    scores = [result['total_score'] for result in results_infos]
+    average_score = sum(scores) / num_results
+    results_json = {"num_results": num_results, "average_score": average_score, "results": results_infos}
+    print(f"Number of results: {num_results}")
+    print(f"Average score: {average_score}")
+    with open(output_file, 'w') as json_file:
+        json.dump(results_json, json_file, indent=4)
 
 
 # Parameters
@@ -19,9 +25,9 @@ output_dir = f'./benchmark/output/{experiments}'
 gold_dir = './benchmark/gold'
 eval_json = 'benchmark/configs/evaluation_ML.jsonl'
 # eval_json = 'benchmark/configs/Evaluation_Visual.jsonl'
-result_json = f'./benchmark/results/{experiments}_result.json'
-timeout_seconds = 30
+output_file = f'./benchmark/results/{experiments}_result.json'
+timeout_seconds = 60
 
 # Run the evaluation with the specified parameters
-run_evaluation(output_dir, gold_dir, eval_json, result_json, timeout_seconds)
+run_evaluation(output_dir, gold_dir, eval_json, output_file, timeout_seconds)
 
