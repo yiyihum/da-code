2b97712b9a6179a172fc5f64f25f0393bf9be3e1
lfy79001 <843265183@qq.com>
2024-06-11 13:00:19 +0800

    add mixtral llama3 qwen

--- spider2/agent/models.py
+++ spider2/agent/models.py
@@ -9,8 +9,8 @@ from io import BytesIO
 
 from openai import AzureOpenAI
 from typing import Dict, List, Optional, Tuple, Any, TypedDict
-
-
+import dashscope
+from groq import Groq
 import google.generativeai as genai
 import openai
 import requests
@@ -160,7 +160,7 @@ def call_llm(payload):
         else:
             return response.json()['content'][0]['text']
 
-    elif model.startswith("mistral"):
+    elif model.startswith("mixtral"):
         messages = payload["messages"]
         max_tokens = payload["max_tokens"]
         top_p = payload["top_p"]
@@ -181,15 +181,16 @@ def call_llm(payload):
 
         from openai import OpenAI
 
-        client = OpenAI(api_key=os.environ["TOGETHER_API_KEY"],
-                        base_url='https://api.together.xyz',
-                        )
-        logger.info("Generating content with Mistral model: %s", model)
+        client = Groq(
+            api_key=os.environ.get("GROQ_API_KEY"),
+        )
 
         flag = 0
         while True:
             try:
-                if flag > 20: break
+                if flag > 20:
+                    break
+                logger.info("Generating content with model: %s", model)
                 response = client.chat.completions.create(
                     messages=mistral_messages,
                     model=model,
@@ -206,10 +207,139 @@ def call_llm(payload):
                 flag = flag + 1
 
         try:
-            return response.choices[0].message.content
+            return True, response.choices[0].message.content
         except Exception as e:
             print("Failed to call LLM: " + str(e))
-            return ""
+            return False, "Failed to call LLM: " + str(e)
+        
+    elif model == "llama3-70b":
+        messages = payload["messages"]
+        max_tokens = payload["max_tokens"]
+        top_p = payload["top_p"]
+        temperature = payload["temperature"]
+
+        groq_messages = []
+
+        for i, message in enumerate(messages):
+            groq_message = {
+                "role": message["role"],
+                "content": ""
+            }
+
+            for part in message["content"]:
+                groq_message['content'] = part['text'] if part['type'] == "text" else ""
+
+            groq_messages.append(groq_message)
+
+        # The implementation based on Groq API
+        client = Groq(
+            api_key=os.environ.get("GROQ_API_KEY"),
+        )
+
+        flag = 0
+        while True:
+            try:
+                if flag > 20:
+                    break
+                logger.info("Generating content with model: %s", model)
+                response = client.chat.completions.create(
+                    messages=groq_messages,
+                    model="llama3-70b-8192",
+                    max_tokens=max_tokens,
+                    top_p=top_p,
+                    temperature=temperature
+                )
+                break
+            except:
+                if flag == 0:
+                    groq_messages = [groq_messages[0]] + groq_messages[-1:]
+                else:
+                    groq_messages[-1]["content"] = ' '.join(groq_messages[-1]["content"].split()[:-500])
+                flag = flag + 1
+
+        try:
+            return True, response.choices[0].message.content
+        except Exception as e:
+            print("Failed to call LLM: " + str(e))
+            return False, "Failed to call LLM: " + str(e)
+        
+    elif model.startswith("qwen"):
+        messages = payload["messages"]
+        max_tokens = payload["max_tokens"]
+        top_p = payload["top_p"]
+        temperature = payload["temperature"]
+
+        qwen_messages = []
+
+        for i, message in enumerate(messages):
+            qwen_message = {
+                "role": message["role"],
+                "content": []
+            }
+            assert len(message["content"]) in [1, 2], "One text, or one text with one image"
+            for part in message["content"]:
+                qwen_message['content'].append({"text": part['text']}) if part['type'] == "text" else None
+
+            qwen_messages.append(qwen_message)
+
+        flag = 0
+        while True:
+            try:
+                if flag > 20:
+                    break
+                logger.info("Generating content with model: %s", model)
+
+                if model in ["qwen-vl-plus", "qwen-vl-max"]:
+                    response = dashscope.MultiModalConversation.call(
+                        model=model,
+                        messages=qwen_messages,
+                        result_format="message",
+                        max_length=max_tokens,
+                        top_p=top_p,
+                        temperature=temperature
+                    )
+
+                elif model in ["qwen-turbo", "qwen-plus", "qwen-max", "qwen-max-0428", "qwen-max-0403",
+                                    "qwen-max-0107", "qwen-max-longcontext"]:
+                    response = dashscope.Generation.call(
+                        model=model,
+                        messages=qwen_messages,
+                        result_format="message",
+                        max_length=max_tokens,
+                        top_p=top_p,
+                        temperature=temperature
+                    )
+
+                else:
+                    raise ValueError("Invalid model: " + model)
+
+                if response.status_code == HTTPStatus.OK:
+                    break
+                else:
+                    logger.error('Request id: %s, Status code: %s, error code: %s, error message: %s' % (
+                        response.request_id, response.status_code,
+                        response.code, response.message
+                    ))
+                    raise Exception("Failed to call LLM: " + response.message)
+            except:
+                if flag == 0:
+                    qwen_messages = [qwen_messages[0]] + qwen_messages[-1:]
+                else:
+                    for i in range(len(qwen_messages[-1]["content"])):
+                        if "text" in qwen_messages[-1]["content"][i]:
+                            qwen_messages[-1]["content"][i]["text"] = ' '.join(
+                                qwen_messages[-1]["content"][i]["text"].split()[:-500])
+                flag = flag + 1
+
+        try:
+            if model in ["qwen-vl-plus", "qwen-vl-max"]:
+                return True, response['output']['choices'][0]['message']['content'][0]['text']
+            else:
+                return True, response['output']['choices'][0]['message']['content']
+
+        except Exception as e:
+            print("Failed to call LLM: " + str(e))
+            return False, "Failed to call LLM: " + str(e)
 
     elif model.startswith("THUDM"):
         # THUDM/cogagent-chat-hf
